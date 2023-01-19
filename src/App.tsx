5caab2d1ea310cefc899c38fe0226a8cadf52c7d
johnflynn <johnflynn493@gmail.com>
2023-01-18 23:17:04 -0500

    Wip on adding branding

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import { Container } from '@mui/system';
 import { CirclePicker, ColorResult, RGBColor, SwatchesPicker } from 'react-color';
 import axios from 'axios';
 import { ImagePalette, RGBAResult } from './types/ImagePalette';
-import { Grid, Palette } from '@mui/material';
+import { Grid, Palette, Typography } from '@mui/material';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
@@ -20,65 +20,24 @@ import { PaletteState } from './types/Palette';
 import TopDistinctSwatches from './components/TopDistinctSwatches';
 import { useLoadedWasm, WasmProvider } from './context/LoadedWasm';
 import ViewImagePalette from './pages/ViewImagePalette';
-
-function ButtonAppBar({ primary, secondary, tertiary, fourth, fifth }: { primary: string, secondary: string, tertiary: string, fourth: string, fifth: string }) {
-  return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar
-        position="static"
-        sx={{
-          backgroundImage: `linear-gradient(144deg,${primary}, ${secondary}, ${tertiary}, ${fourth}, ${fifth})`
-        }}
-      >
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
-        </Toolbar>
-      </AppBar>
-    </Box>
-  );
-}
-
-function componentToHex(c: number): string {
-  var hex = c.toString(16);
-  return hex.length === 1 ? "0" + hex : hex;
-}
-
-function rgbToHex(r: number, g: number, b: number): string {
-  return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
-}
-
-function rgbResultToHex(clrResult: RGBAResult): string {
-  return rgbToHex(clrResult.R, clrResult.G, clrResult.B)
-}
-
-const newCacheFromResult = (arr: Array<Array<RGBAResult>>): Map<string, RGBAResult> => {
-  const newCache = new Map<string, RGBAResult>()
-
-  arr.forEach((rgbArray) => {
-    rgbArray.forEach((rgb) => {
-      newCache.set(rgbToHex(rgb.R, rgb.G, rgb.B), rgb)
-    })
-  })
-
-  return newCache
-}
+import { rgbResultToHex } from './utils/colorUtils';
+import { ReactComponent as Logo } from './assets/logo.svg'
 
 type GoFns = {
   SayHi?: () => void,
 }
 
-function TheComponent() {
-  const wasm = useLoadedWasm<GoFns>()
-  wasm?.SayHi?.()
-  return <p>{wasm ? 'Loaded!! ' : 'Loading....'}</p>
+function Heading() {
+  return <Box sx={{ flexGrow: 1 }}>
+    <AppBar style={{ backgroundColor: '#333340' }} position="static">
+      <Toolbar>
+        <Logo style={{ width: '40px', height: '40px' }} />
+        <Typography>
+          Pic Palette
+        </Typography>
+      </Toolbar>
+    </AppBar>
+  </Box>
 }
 
 function App() {
@@ -100,9 +59,12 @@ function App() {
 
   return <ThemeProvider theme={theme}>
     <WasmProvider fetchParams="go-wasm.wasm">
-      <ViewImagePalette imagePalette={imagePalette} setImagePalette={setImagePalette} />
+      {/* <ViewImagePalette imagePalette={imagePalette} setImagePalette={setImagePalette} /> */}
+      <Heading />
+      <p>Hello!</p>
     </WasmProvider>
   </ThemeProvider>
 }
 
 export default App;
+
