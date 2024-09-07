03f0cda90d6fdfb05da831f9711f1ba5e4e8e4b4
johnflynn <johnflynn493@gmail.com>
2024-09-07 10:24:56 -0400

    Updated cli.go

--- go/cmd/cli.go
+++ go/cmd/cli.go
@@ -15,6 +15,6 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	} else {
-		fmt.Print(GetJsonForImage(imgData, numberOfColors, numberOfTopDistincts))
+		fmt.Print(GetJsonForImage(imgData, *numberOfColors, *numberOfTopDistincts))
 	}
 }
